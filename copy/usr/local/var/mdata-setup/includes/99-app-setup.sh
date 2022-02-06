0f3c430eb16766330eaeba4dccd827ed4ad2ea24
jfqd <jfqd@blun.org>
2022-02-06 12:50:11 +0100

    refactor coturn config

--- copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-coturn_pwd
+IP=$(ifconfig eth0 |grep 'inet ' |awk '{print $2}')
+HOSTNAME=$(hostname -f)
+DOMAIN=$(hostname -d)
 
 if /native/usr/sbin/mdata-get coturn_pwd 1>/dev/null 2>&1; then
   NEW_PWD=$(/native/usr/sbin/mdata-get coturn_pwd)
 else
   NEW_PWD=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | shasum -a 256 | awk '{print $1}' | tr -d '\n')
 fi
-sed -i "s/#static-auth-secret=north/static-auth-secret=${NEW_PWD}/" /etc/turnserver.conf
 
-HOSTNAME=$(hostname -f)
-sed -i "s/#server-name=blackdow.carleon.gov/server-name=${HOSTNAME}/" /etc/turnserver.conf
-
-DOMAIN=$(hostname -d)
-sed -i "s/#realm=mycompany.org/realm=${DOMAIN}/" /etc/turnserver.conf
-
-sed -i "s|#cert=/usr/local/etc/turn_server_cert.pem|cert=/etc/letsencrypt/live/${HOSTNAME}/fullchain.pem|" /etc/turnserver.conf
-sed -i "s|#pkey=/usr/local/etc/turn_server_pkey.pem|pkey=/etc/letsencrypt/live/${HOSTNAME}/privkey.pem|" /etc/turnserver.conf
-
-IP=$(ifconfig eth0 |grep 'inet ' |awk '{print $2}')
-sed -i "s/#listening-ip=172.17.19.101/listening-ip=${IP}/" /etc/turnserver.conf
-sed -i "s/#relay-ip=172.17.19.105/relay-ip=${IP}/" /etc/turnserver.conf
+sed -i \
+    -e "s/#tls-listening-port=5349/tls-listening-port=5349/" \
+    -e "s/#listening-ip=172.17.19.101/listening-ip=${IP}/" \
+    -e "s/#relay-ip=172.17.19.105/relay-ip=${IP}/" \
+    -e "s/#fingerprint/fingerprint/" \
+    -e "s/#use-auth-secret/use-auth-secret/" \
+    -e "s/#static-auth-secret=north/static-auth-secret=${NEW_PWD}/" \
+    -e "s/#server-name=blackdow.carleon.gov/server-name=${HOSTNAME}/" \
+    -e "s/#realm=mycompany.org/realm=${DOMAIN}/" \
+    -e "s/#total-quota=0/total-quota=100/" \
+    -e "s/# bps-capacity=0/bps-capacity=0/" \
+    -e "s|#cert=/usr/local/etc/turn_server_cert.pem|cert=/etc/letsencrypt/live/${HOSTNAME}/fullchain.pem|" \
+    -e "s|#pkey=/usr/local/etc/turn_server_pkey.pem|pkey=/etc/letsencrypt/live/${HOSTNAME}/privkey.pem|" \
+    -e "s/#cipher-list=\"DEFAULT\"/cipher-list=\"ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!AECDH:!MD5\"/" \
+    -e "s/#dh-file=<DH-PEM-file-name>/dh-file=/etc/dh2048.pem/" \
+    -e "s/#no-stdout-log/no-stdout-log/" \
+    -e "s/#no-multicast-peers/no-multicast-peers/" \
+    -e "s/#no-tlsv1/no-tlsv1/" \
+    -e "s/#no-tlsv1_1/no-tlsv1_1/" \
+    /etc/turnserver.conf
 
 openssl dhparam -out /etc/dh2048.pem 2048
 
-service coturn restart
+systemctl stop coturn || true
+sleep 5
+systemctl start coturn || true
